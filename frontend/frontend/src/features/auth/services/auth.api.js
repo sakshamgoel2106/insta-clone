d9283698b17c65af40b2cbff1a302eaf0ffc89f0
Your Name <your@email.com>
2026-08-24 04:23:50 +0530

    changes

--- frontend/frontend/src/features/auth/services/auth.api.js
+++ frontend/frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ||
+    (import.meta.env.DEV ? "http://localhost:5000" : "https://insta-clone-wah1.onrender.com");
+
 const api = axios.create({
-    baseURL: `${import.meta.env.VITE_API_BASE_URL || "http://localhost:5000"}/api/auth`,
+    baseURL: `${BASE_URL}/api/auth`,
     withCredentials:true
 })
 export async function register (username,email,password){
--- frontend/frontend/src/features/post/services/post.api.js
+++ frontend/frontend/src/features/post/services/post.api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL ||
+    (import.meta.env.DEV ? "http://localhost:5000" : "https://insta-clone-wah1.onrender.com");
 
 const api = axios.create({
     baseURL: `${BASE_URL}/api/post`,
